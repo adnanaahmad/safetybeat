aed9f47044666507fd458f6890c3ea1f391bffda
Rana Asad Fiaz <ranaasadfiaz@gmail.com>
2018-11-29 09:29:14 +0500

    Rooot component and login component routes towards login page

--- src/components/login/login.component.ts
+++ src/components/login/login.component.ts
@@ -3,7 +3,10 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { first } from 'rxjs/operators';
 
-@Component({ templateUrl: 'login.component.html' })
+@Component({
+  templateUrl: 'login.component.html',
+  selector: 'app-login',
+})
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
   loading = false;
