c9230e10b1de20bd7a00a4db95d2bb866025efbd
Muttaqeen Rizvi <m.rizvi@optergy.com>
2018-12-10 18:07:04 +0500

    new project structure

---
+++ src/app/pages/dashboard/services/organization.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+// import { environment } from '../../environments/environment';
+// import { AuthService } from '../auth/auth.service';
+import { HttpHeaders } from '@angular/common/http';
+import { CookieService } from 'ngx-cookie-service';
+// import { TokenInterceptorService } from '../auth/token-interceptor';
+@Injectable({ providedIn: 'root' })
+export class OrganizationService {
+    // constructor(private http: HttpClient, public auth: AuthService) {
+    // }
+
+    companyType() {
+        // return this.http.get(`${environment.apiUrl}/anonymous/companyTypes/`);
+    }
+}
