1e0ab0483988efa3f7eb356f8b6fa14c3fb09c9d
Muttaqeen Rizvi <m.rizvi@optergy.com>
2018-12-19 10:09:45 +0500

    profile page structure done

--- src/app/pages/profile/profile.module.ts
+++ src/app/pages/profile/profile.module.ts
@@ -5,12 +5,14 @@ import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { UserComponent } from './pages/user/user.component';
 import { OrganizationComponent } from './pages/organization/organization.component';
+import { MaterialModule } from 'src/app/shared/material/material.module';
 
 @NgModule({
   declarations: [ProfileComponent, UserComponent, OrganizationComponent],
   imports: [
     CommonModule,
-    ProfileRoutingModule
+    ProfileRoutingModule,
+    MaterialModule
   ]
 })
 export class ProfileModule { }
