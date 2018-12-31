fb56ee077b2d382d444d6903a78c3624d945e07a
Rana Asad Fiaz <ranaasadfiaz@gmail.com>
2018-12-31 09:05:03 +0500

    preparing to take pull from Development branch

--- src/app/pages/auth/pages/login/login.component.spec.ts
+++ src/app/pages/auth/pages/login/login.component.spec.ts
@@ -1,19 +1,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Injectable } from '@angular/core';
 import { LoginComponent } from './login.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { MaterialModule } from 'src/app/shared/material/material.module';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { createTranslateLoader } from 'src/app/app.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrManager, ToastrModule } from 'ng6-toastr-notifications';
+import { AuthService } from 'src/app/core/auth/auth.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { DebugElement } from '@angular/core';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
+  let authService: AuthService;
+  let debugEl: DebugElement;
+  let nativeEl: HTMLElement;
   let fixture: ComponentFixture<LoginComponent>;
-
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [LoginComponent],
@@ -34,19 +39,28 @@ describe('LoginComponent', () => {
       ]
     })
       .compileComponents();
+    authService = TestBed.get(AuthService);
+  }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, RouterTestingModule, ReactiveFormsModule, FormsModule],
+      declarations: [LoginComponent],
+      providers: [
+        { provide: AuthService, },
+      ]
+    }).compileComponents();
+
+    authService = TestBed.get(AuthService);
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    debugEl = fixture.debugElement;
+    nativeEl = fixture.nativeElement;
   });
 
   it('should create', () => {
-    // tslint:disable-next-line:no-shadowed-variable
-    const fixture = TestBed.createComponent(LoginComponent);
-    // tslint:disable-next-line:no-shadowed-variable
-    const component = fixture.debugElement.componentInstance;
     expect(component).toBeTruthy();
   });
 });
