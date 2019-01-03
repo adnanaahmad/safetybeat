f42c43441238d0662c122e6c110aba27e7ea18a2
Sohaib Amjad <s.amjad@optergy.com>
2019-01-03 10:42:57 +0500

    Particle Container Component Created

---
+++ src/app/pages/login-Registration/components/particle-container/particle-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-particle-container',
+  templateUrl: './particle-container.component.html',
+  styleUrls: ['./particle-container.component.scss']
+})
+export class ParticleContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
