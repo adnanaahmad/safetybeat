83bc5c2fa84986dcb6f97d01f53674c151a2a44b
Muttaqeen Rizvi <m.rizvi@optergy.com>
2019-01-04 17:55:06 +0500

    theming is done for light theme , logger for users is implement but missing in some componentes,

--- src/app/pages/login-Registration/components/forgot-password/forgot-password.component.ts
+++ src/app/pages/login-Registration/components/forgot-password/forgot-password.component.ts
@@ -6,6 +6,7 @@ import { TranslateService } from '@ngx-translate/core';
 import { ForgotPassword } from 'src/app/models/user.model';
 import { LoginRegistrationService } from '../../services/LoginRegistrationService';
 import { LoggingService } from 'src/app/shared/logging/logging.service';
+import { Translation } from 'src/app/models/translate.model';
 
 @Component({
   selector: 'app-forgot-password',
@@ -14,50 +15,28 @@ import { LoggingService } from 'src/app/shared/logging/logging.service';
 })
 export class ForgotPasswordComponent implements OnInit {
   forgotPassForm: FormGroup;
-  translated: object;
+  translated: Translation;
   selectedTheme: String;
-  status: string;
-  warning: string;
-  info: string;
-  error: string;
-  success: string;
-  forgot_req: string;
-  default: string;
-  forgotsuccess: string;
   constructor(
     public forgotService: LoginRegistrationService,
     private router: Router,
     public formBuilder: FormBuilder,
     public translate: TranslateService,
     private logging: LoggingService
-  )
-  /**
-   *in this translate.get function i have subscribed the en.json AUTH,BUTTONS and MESSAGES strings and have used in the html
-   *file
-   */
-  // tslint:disable-next-line:one-line
-  {
+  ) {
     translate.get(['AUTH', 'BUTTONS', 'MESSAGES', 'LOGGER']).subscribe((values) => {
       this.translated = values;
-      this.default = values.LOGGER.STATUS.DEFAULT;
-      this.info = values.LOGGER.STATUS.INFO;
-      this.success = values.LOGGER.STATUS.SUCCESS;
-      this.warning = values.LOGGER.STATUS.WARNING;
-      this.error = values.LOGGER.STATUS.ERROR;
-      this.forgot_req = values.LOGGER.MESSAGES.FORGOT_REQ;
-      this.forgotsuccess = values.LOGGER.MESSAGES.FORGOTSUCCESS;
-      this.status = values.LOGGER.MESSAGES.STATUS;
     });
+    this.logging.appLogger(this.translated.LOGGER.STATUS.SUCCESS, this.translated.LOGGER.MESSAGES.FORGOT_COMPONENT);
   }
   ngOnInit()
-  /**
-   *here we are checking the validity of email
-   */
-  // tslint:disable-next-line:one-line
   {
     this.forgotPassForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]]
     });
+  }
+  ngOnDestroy(){
+    this.logging.hideAllAppLoggers()
   }
   /**
    * in this function loginform controls are checked whether they are valid or not and this is basically builtin fucntionality
@@ -69,19 +48,19 @@ export class ForgotPasswordComponent implements OnInit {
    */
   onSubmit({ value, valid }: { value: ForgotPassword; valid: boolean }): void {
     if (!valid) {
-      this.logging.appLogger(this.warning, valid);
-      this.logging.appLogger(this.error, this.forgot_req);
+      this.logging.appLogger(this.translated.LOGGER.STATUS.WARNING, valid);
+      this.logging.appLogger(this.translated.LOGGER.STATUS.ERROR, this.translated.LOGGER.MESSAGES.FORGOT_REQ);
       return;
     }
-    this.logging.appLogger(this.info, valid);
-    this.logging.appLogger(this.info, JSON.stringify(value));
+    this.logging.appLogger(this.translated.LOGGER.STATUS.INFO, valid);
+    this.logging.appLogger(this.translated.LOGGER.STATUS.INFO, JSON.stringify(value));
     this.forgotService.forgotPassword(value).subscribe(
       data => {
-        this.logging.appLogger(this.success, this.forgotsuccess);
+        this.logging.appLogger(this.translated.LOGGER.STATUS.SUCCESS, this.translated.LOGGER.MESSAGES.FORGOTSUCCESS);
         this.router.navigate(['/login']);
       },
       error => {
-        this.logging.appLogger(this.error, `${this.status + error.status}`);
+        this.logging.appLogger(this.translated.LOGGER.STATUS.ERROR, `${this.translated.LOGGER.MESSAGES.STATUS + error.status}`);
       }
     );
 
