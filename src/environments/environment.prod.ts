f150087a9078d6d598f5633c29c1a0bbef0ca9e1
Sohaib Amjad <s.amjad@optergy.com>
2019-01-24 18:37:50 +0500

    Customized all the themes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
-const serverUrl = 'http://192.168.50.62:8000';
+const serverUrl = 'http://192.168.50.93:8000';
 // const serverUrl = 'http://localhost:3000';
 export const environment = {
   production: true,
