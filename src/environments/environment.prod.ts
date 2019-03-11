1531c6e26054ab940ed12136c3de1a8783e06cfb
Sohaib Amjad <s.amjad@optergy.com>
2019-03-11 17:04:48 +0500

    Thora sa change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
-const serverUrl = 'http://192.168.69.5:8000';
+const serverUrl = 'http://192.168.69.67';
 export const environment = {
   production: true,
-  apiUrl: `${serverUrl}/safetybeat`,
+  apiUrl: `${serverUrl}/api/safetybeat`,
 };
 
