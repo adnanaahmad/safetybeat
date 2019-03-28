7a2cbdc36d3bce256ab3d9041d591159429d415a
Muttaqeen Rizvi <m.rizvi@optergy.com>
2019-03-28 17:40:56 +0500

    fix

--- src/app/pages/loginRegistration/loginRegister-routing.module.ts
+++ src/app/pages/loginRegistration/loginRegister-routing.module.ts
@@ -34,12 +34,6 @@ const authRoutes: Routes = [
     path: "signup",
     component: RegistrationComponent
   },
-  // {
-  //   path: "verification",
-  //   component: VerificationComponent,
-  //   canActivate: [AuthGuard],
-  //   canDeactivate: ["canDeactivateVerification"]
-  // }
 ];
 
 @NgModule({
