b6fcca8af9d51974574cec7863bc5e296806d2c2
Muttaqeen Rizvi <m.rizvi@optergy.com>
2019-04-11 16:13:54 +0500

    merge

--- src/app/directives/googlePlaces/googlePlaces.directive.ts
+++ src/app/directives/googlePlaces/googlePlaces.directive.ts
@@ -1,5 +1,5 @@
 /// <reference types='@types/googlemaps' />
-import { Directive, OnInit, Output, EventEmitter, ElementRef } from '@angular/core';
+import {Directive, OnInit, Output, EventEmitter, ElementRef} from '@angular/core';
 
 @Directive({
   selector: '[appGooglePlaces]'
@@ -7,28 +7,31 @@ import { Directive, OnInit, Output, EventEmitter, ElementRef } from '@angular/co
 export class GooglePlacesDirective implements OnInit {
   @Output() onSelect: EventEmitter<any> = new EventEmitter();
   private element: HTMLInputElement;
+
   constructor(elRef: ElementRef) {
     this.element = elRef.nativeElement;
   }
+
   getFormattedAddress(place) {
     let location_obj = {};
     for (let i in place.address_components) {
       let item = place.address_components[i];
       location_obj['formatted_address'] = place.formatted_address;
-      if (item['types'].indexOf("locality") > -1) {
+      if (item['types'].indexOf('locality') > -1) {
         location_obj['locality'] = item['long_name']
-      } else if (item['types'].indexOf("administrative_area_level_1") > -1) {
+      } else if (item['types'].indexOf('administrative_area_level_1') > -1) {
         location_obj['State'] = item['short_name']
-      } else if (item['types'].indexOf("street_number") > -1) {
+      } else if (item['types'].indexOf('street_number') > -1) {
         location_obj['street_number'] = item['short_name']
-      } else if (item['types'].indexOf("country") > -1) {
+      } else if (item['types'].indexOf('country') > -1) {
         location_obj['country'] = item['long_name']
-      } else if (item['types'].indexOf("postal_code") > -1) {
+      } else if (item['types'].indexOf('postal_code') > -1) {
         location_obj['zipCode'] = item['short_name']
       }
     }
     return location_obj;
   }
+
   ngOnInit() {
     const autocomplete = new google.maps.places.Autocomplete(this.element);
     google.maps.event.addListener(autocomplete, 'place_changed', () => {
