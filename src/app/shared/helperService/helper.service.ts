6f6de76e962e092fadb4e8d6eb92298a2112aa89
Khadeeja <k.basharat@optergy.com>
2019-04-29 13:30:24 +0500

    Address issue fixed. Issue was with Ngzone.

--- src/app/shared/helperService/helper.service.ts
+++ src/app/shared/helperService/helper.service.ts
@@ -1,4 +1,4 @@
-import {Injectable, ElementRef} from '@angular/core';
+import {Injectable, ElementRef, NgZone} from '@angular/core';
 import {forEach, findIndex, remove, sortBy} from 'lodash';
 import {TranslateService} from '@ngx-translate/core';
 import {Translation} from 'src/app/models/translate.model';
@@ -40,7 +40,8 @@ export class HelperService {
     public dialog: MatDialog,
     private cookies: CookieService,
     private router: Router,
-    private notifier: NotifierService
+    private notifier: NotifierService,
+    private ngZone: NgZone
   ) {
     translate.get(['AUTH', 'BUTTONS', 'MESSAGES', 'LOGGER', 'STRINGS', 'ICONS', 'SITETITLE',
       'STATUS', 'TABLEHEADINGS', 'CONFIRMATION']).subscribe((values) => {
@@ -232,21 +233,22 @@ export class HelperService {
   setLocationGeocode(address, mapProp) {
     let geoCoder = new google.maps.Geocoder();
     let self = this;
-    let promise = new Promise((resolve, reject) => {
-      geoCoder.geocode({'address': address}, function (results, status) {
-        if (status.toString() === self.constants.status.OK) {
-          mapProp.setCenter(results[0].geometry.location);
-          let marker = new google.maps.Marker({
-            map: mapProp,
-            position: results[0].geometry.location
-          });
-          resolve(true);
-        } else {
-          reject(false);
-        }
+    return new Promise((resolve, reject) => {
+      this.ngZone.run(() => {
+        geoCoder.geocode({'address': address}, function (results, status) {
+          if (status.toString() === self.constants.status.OK) {
+            mapProp.setCenter(results[0].geometry.location);
+            let marker = new google.maps.Marker({
+              map: mapProp,
+              position: results[0].geometry.location
+            });
+            resolve(true);
+          } else {
+            reject(false);
+          }
+        });
       });
     });
-    return promise;
   }
 
   /**
