293e63c360fc714f70f7908a5474bc0b27c4db93
Sohaib Amjad <s.amjad@optergy.com>
2019-05-06 17:19:45 +0500

    Navigation changes

--- src/app/pages/navigation/components/fixedNav/fixedNav.component.ts
+++ src/app/pages/navigation/components/fixedNav/fixedNav.component.ts
@@ -1,7 +1,7 @@
-import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
-import {NavItem} from 'src/app/models/navItems.model';
-import {HelperService} from 'src/app/shared/helperService/helper.service';
-import {FixedNavModel} from 'src/app/models/navigation/fixedNav.model';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { NavItem } from 'src/app/models/navItems.model';
+import { HelperService } from 'src/app/shared/helperService/helper.service';
+import { FixedNavModel } from 'src/app/models/navigation/fixedNav.model';
 
 @Component({
   selector: 'app-fixed-nav',
@@ -24,9 +24,39 @@ export class FixedNavComponent implements OnInit {
       toolTip: 'Dashboard'
     },
     {
+      route: '/home/profile/user',
+      iconName: this.helperService.constants.appIcons.supervisedUserCircle,
+      toolTip: 'Users'
+    },
+    {
       route: '/home/adminControl/entityControl',
-      iconName: this.helperService.constants.appIcons.contacts,
+      iconName: this.helperService.constants.appIcons.log,
       toolTip: 'Entity Control Center'
+    },
+    {
+      route: '/home/adminControl/memberCenter',
+      iconName: this.helperService.constants.appIcons.group,
+      toolTip: 'Member Center'
+    },
+    {
+      route: '/home/adminControl/siteCenter',
+      iconName: this.helperService.constants.appIcons.domain,
+      toolTip: 'Site Center'
+    },
+    {
+      route: '/home/adminControl/questionCenter',
+      iconName: this.helperService.constants.appIcons.help,
+      toolTip: 'Question Center'
+    },
+    {
+      route: '/home/adminControl/hazardCenter',
+      iconName: this.helperService.constants.appIcons.warning,
+      toolTip: 'Hazard Center'
+    },
+    {
+      route: '/home/documents',
+      iconName: this.helperService.constants.appIcons.insertDriveFile,
+      toolTip: 'Documents'
     }
   ];
   public navLinksBottom: NavItem[] = [
