c52b273f4ed2db7413b224c882ad5df8935a07c0
Sohaib Amjad <s.amjad@optergy.com>
2019-05-09 16:03:21 +0500

    Latest UI fixes

--- src/app/pages/adminControl/modules/siteCenter/dialogs/addHazard/addHazard.component.ts
+++ src/app/pages/adminControl/modules/siteCenter/dialogs/addHazard/addHazard.component.ts
@@ -1,9 +1,9 @@
-import {Component, Inject, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {HelperService} from 'src/app/shared/helperService/helper.service';
-import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
-import {AddHazardModel, NewHazard} from 'src/app/models/hazard.model';
-import {AdminControlService} from 'src/app/pages/adminControl/services/adminControl.service';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HelperService } from 'src/app/shared/helperService/helper.service';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { AddHazardModel, NewHazard } from 'src/app/models/hazard.model';
+import { AdminControlService } from 'src/app/pages/adminControl/services/adminControl.service';
 
 @Component({
   selector: 'app-addHazard',
@@ -14,12 +14,15 @@ export class AddHazardComponent implements OnInit {
   private addHazardForm: FormGroup;
   hazardObj: AddHazardModel = <AddHazardModel>{};
   risks: string[];
+  fileInputTextDiv: any;
+  fileInput: any;
+  fileInputText: any;
 
   constructor(public formBuilder: FormBuilder,
-              public helperService: HelperService,
-              public service: AdminControlService,
-              @Inject(MAT_DIALOG_DATA) public data: any,
-              public dialogRef: MatDialogRef<AddHazardComponent>) { }
+    public helperService: HelperService,
+    public service: AdminControlService,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    public dialogRef: MatDialogRef<AddHazardComponent>) { }
 
   ngOnInit() {
     this.addHazardForm = this.formBuilder.group({
@@ -29,14 +32,39 @@ export class AddHazardComponent implements OnInit {
     });
     this.service.getHazards().subscribe((res) => {
       this.risks = res;
-      }, (error) => {
-        this.addHazardForm.disable();
-        this.helperService.createSnack('Hazard list could not be added',
-          this.helperService.constants.status.ERROR);
-      }
+    }, (error) => {
+      this.addHazardForm.disable();
+      this.helperService.createSnack('Hazard list could not be added',
+        this.helperService.constants.status.ERROR);
+    }
     );
+    this.fileInputTextDiv = document.getElementById('file_input_text_div');
+    this.fileInput = document.getElementById('file_input_file');
+    this.fileInputText = document.getElementById('file_input_text');
+    this.fileInput.addEventListener('change', this.changeInputText);
+    this.fileInput.addEventListener('change', this.changeState);
   }
-
+  changeInputText() {
+    var str = this.fileInput.value;
+    var i;
+    if (str.lastIndexOf('\\')) {
+      i = str.lastIndexOf('\\') + 1;
+    } else if (str.lastIndexOf('/')) {
+      i = str.lastIndexOf('/') + 1;
+    }
+    this.fileInputText.value = str.slice(i, str.length);
+  }
+  changeState() {
+    if (this.fileInputText.value.length != 0) {
+      if (!this.fileInputTextDiv.classList.contains("is-focused")) {
+        this.fileInputTextDiv.classList.add('is-focused');
+      }
+    } else {
+      if (this.fileInputTextDiv.classList.contains("is-focused")) {
+        this.fileInputTextDiv.classList.remove('is-focused');
+      }
+    }
+  }
   get addHazardControls() {
     return this.addHazardForm.controls;
   }
@@ -45,8 +73,8 @@ export class AddHazardComponent implements OnInit {
     this.hazardObj.image = <File>event.target.files[0];
   }
 
-  addHazard({value, valid}: { value: NewHazard; valid: boolean }): void {
-    let blob = new Blob([this.hazardObj.image], {type: 'application/image'});
+  addHazard({ value, valid }: { value: NewHazard; valid: boolean }): void {
+    let blob = new Blob([this.hazardObj.image], { type: 'application/image' });
     let formData = new FormData();
     formData.append('image', blob, this.hazardObj.image.name);
     formData.append('title', value.title);
@@ -58,14 +86,14 @@ export class AddHazardComponent implements OnInit {
       return;
     }
     this.service.addNewHazard(formData).subscribe((res) => {
-        this.helperService.createSnack('Hazard Added', this.helperService.constants.status.SUCCESS);
-        this.onNoClick();
-        this.helperService.createSnack(this.helperService.translated.MESSAGES.HAZARD_ADDED, this.helperService.constants.status.SUCCESS);
+      this.helperService.createSnack('Hazard Added', this.helperService.constants.status.SUCCESS);
+      this.onNoClick();
+      this.helperService.createSnack(this.helperService.translated.MESSAGES.HAZARD_ADDED, this.helperService.constants.status.SUCCESS);
 
-      }, (error) => {
-        this.helperService.createSnack(this.helperService.translated.MESSAGES.HAZARD_NOT_ADDED,
-          this.helperService.constants.status.ERROR);
-      }
+    }, (error) => {
+      this.helperService.createSnack(this.helperService.translated.MESSAGES.HAZARD_NOT_ADDED,
+        this.helperService.constants.status.ERROR);
+    }
     );
   }
   onNoClick(): void {
