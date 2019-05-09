6d2d101ef874ebb3dbcfb414615995c5be21e85c
Khadeeja <k.basharat@optergy.com>
2019-05-09 11:36:30 +0500

    Working on view all connections.

--- src/app/pages/adminControl/modules/memberCenter/dialogs/viewConnections/viewConnections.component.ts
+++ src/app/pages/adminControl/modules/memberCenter/dialogs/viewConnections/viewConnections.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { HelperService } from 'src/app/shared/helperService/helper.service';
+import {Component, OnInit} from '@angular/core';
+import {HelperService} from 'src/app/shared/helperService/helper.service';
+import {AdminControlService} from '../../../../services/adminControl.service';
 
 @Component({
   selector: 'app-viewConnections',
@@ -8,11 +9,19 @@ import { HelperService } from 'src/app/shared/helperService/helper.service';
 })
 export class ViewConnectionsComponent implements OnInit {
 
+  connections = [];
+
   constructor(
-    public helperService: HelperService
-  ) { }
+    public helperService: HelperService,
+    private adminService: AdminControlService
+  ) {
+  }
 
   ngOnInit() {
+    this.adminService.allConnections().subscribe(res => {
+      console.log(res);
+      this.connections = res.data;
+    });
   }
 
 }
