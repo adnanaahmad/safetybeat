043ee1046994a1ac484ded31734cf328a7274b98
tehreem <t.rasool@optergy.com>
2019-07-22 15:14:27 +0500

    taking pull

---
+++ src/app/features/adminControl/modules/siteCenter/dialogs/addActions/addActions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-actions',
+  templateUrl: './addActions.component.html',
+  styleUrls: ['./addActions.component.scss']
+})
+export class AddActionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
