2b866eeb05543097d4d49f0805a7a059fe1ac881
asadfiaz <a.fiaz@optergy.com>
2019-07-30 11:27:25 +0500

    taking pull from testDevelopment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 
-const serverUrl = 'http://192.168.15.174:8000';
+const serverUrl = 'http://localhost:8000';
 
 export const environment = {
   production: false,
